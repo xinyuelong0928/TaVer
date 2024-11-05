912ba052921626e18a3f4c6c4788bd983b54765e
xinyuelong0928 <address>
2024-11-05 20:46:20 +0800

    Add files via upload

---
+++ data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python data_process.py  --language=c \
+                        \
+                        \
+                        --data_file=./datasets \
+                        --pkl_file=./utils/pkl_folder
---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+python ./utils/evaluator.py --fusion_languages=c_php \
+                            --target_language=php \
+                            \
+                            \
+                            --output_dir=./results \
+                            -a ./datasets
+
+
+
+#  Acc: xxx ,Recall: xxx ,Prediction: xxx ,F1: xxx
