d193d5f3e7b0ef08b896d5b0a17e02e5975a23f5
xinyuelong0928 <bbaa70926@gmail.com>
2024-12-31 12:51:24 +0800

    Delete data.sh

--- data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python data_process.py  --language=c \
-                        \
-                        \
-                        --data_file=./datasets \
-                        --pkl_file=./utils/pkl_folder
